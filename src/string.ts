40928ed606c5c09848f97649468980ff416d8a52
WMXPY <wm@mengw.io>
2021-06-20 11:51:46 -0500

    add date strinfiy

--- src/string.ts
+++ src/string.ts
@@ -4,14 +4,21 @@
  * @description String
  */
 
+export enum CSV_DATE_CASE_METHOD {
+
+    MILLISECONDS_SINCE_ELAPSED = "MILLISECONDS_SINCE_ELAPSED",
+}
+
 export type CSVCellToStringOptions = {
 
     readonly nullReplacement: string;
+    readonly dateCastMethod: CSV_DATE_CASE_METHOD;
 };
 
 const defaultCellToStringOptions: CSVCellToStringOptions = {
 
     nullReplacement: 'null',
+    dateCastMethod: CSV_DATE_CASE_METHOD.MILLISECONDS_SINCE_ELAPSED,
 };
 
 export const csvCellToString = (cell: any, options: CSVCellToStringOptions = defaultCellToStringOptions): string => {
@@ -22,6 +29,15 @@ export const csvCellToString = (cell: any, options: CSVCellToStringOptions = def
 
     if (cell === null) {
         return options.nullReplacement;
+    }
+
+    if (cell instanceof Date) {
+        switch (options.dateCastMethod) {
+            case CSV_DATE_CASE_METHOD.MILLISECONDS_SINCE_ELAPSED:
+                return cell.getTime().toString();
+            default:
+                return cell.getTime().toString();
+        }
     }
 
     if (cell.toString) {
